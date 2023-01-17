c3ed2f66b65e63dc5f77e6fa71c9dc1dae02d41f
cha <chyb627@naver.com>
2023-01-17 18:44:41 +0900

    버튼 추가

--- scr/screen/ImageDetailScreen.tsx
+++ scr/screen/ImageDetailScreen.tsx
@@ -1,8 +1,11 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
 import React, { useCallback } from 'react';
 import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { Button } from '../components/Button';
 import { Header } from '../components/Header/Header';
+import { Icon } from '../components/Icons';
 import { RemoteImage } from '../components/RemoteImage';
+import { Typography } from '../components/Typography';
 
 export const ImageDetailScreen = (props) => {
   const navigation = useNavigation();
@@ -12,6 +15,10 @@ export const ImageDetailScreen = (props) => {
     navigation.goBack();
   }, []);
   const { width } = useWindowDimensions();
+
+  const onPressDownload = useCallback(() => {
+    console.log('download');
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -26,6 +33,21 @@ export const ImageDetailScreen = (props) => {
       <View style={styles.item}>
         <RemoteImage url={route.params.url} width={width} height={width * 1.5} />
       </View>
+
+      <Button onPress={onPressDownload}>
+        <View style={{ paddingBottom: 24, backgroundColor: 'black' }}>
+          <View
+            style={{
+              height: 52,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Typography color={'white'}>DOWNLOAD</Typography>
+            <Icon name="download" size={24} color="white" />
+          </View>
+        </View>
+      </Button>
     </View>
   );
 };
